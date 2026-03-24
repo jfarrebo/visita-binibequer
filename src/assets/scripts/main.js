9e334d033f73f599c132d4cc5d2aca633fab7100
Judith Farre Bonet <jfarrebo@uoc.edu>
2026-03-24 20:47:10 +0100

    Creació estils

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,10 @@
  * Write any other JavaScript below
  */
 
-+(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-})();
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration: 800,
+  once: true,
+});
